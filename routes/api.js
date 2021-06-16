67e5d81439a9d44d99fa7e604429e1fa4ac5d233
MykalB <mykalb081@gmail.com>
2021-06-16 13:38:50 -0400

    Update api.js
    - Add route for conversions
    - use convertHandlers methods to handle conversions

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,22 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+  app.route('/api/convert?').get((req, res) => {
+    let { input } = req.query;
+
+    let num = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let returnNum = convertHandler.convert(num, initUnit);
+    let toString = convertHandler.getString(num, initUnit, returnNum, returnUnit);
+
+    res.json({
+      initNum: Number(num),
+      initUnit: initUnit,
+      returnNum: Number(returnNum),
+      returnUnit: returnUnit,
+      string: toString
+    });
+  });
 
 };
